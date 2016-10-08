478bcb4da5bec39209c0d5ff1562b370b75884a7
Brian Provost <brianmprovost@gmail.com>
2016-10-07 23:37:16 -0500

    Project setup.

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
+};
